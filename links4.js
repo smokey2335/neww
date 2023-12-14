7a6a1fde660e7f08d3b48da8c8201f851c0eb64e
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2023-12-14 15:23:27 +0100

    Create links4.js

---
+++ links4.js
@@ -0,0 +1,137 @@
+// First function
+function openFirstPopup() {
+    var popup = window.open('https://bit.ly/julie---jess', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/julie---jess', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Second function
+function openSecondPopup() {
+    var popup = window.open('https://bit.ly/mia-_melano', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/mia-_melano', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Third function
+function openThirdPopup() {
+    var popup = window.open('https://bit.ly/lacy---lennon', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/lacy---lennon', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Fourth function
+function openFourthPopup() {
+    var popup = window.open('https://bit.ly/sonya---blaze', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/sonya---blaze', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Fifth function
+function openFifthPopup() {
+    var popup = window.open('https://bit.ly/nikki---benz', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/nikki---benz', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// sixth function
+function openSixthPopup() {
+    var popup = window.open('https://bit.ly/lisa---ann', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/lisa---ann', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// seventh function
+function openSeventhPopup() {
+    var popup = window.open('https://bit.ly/kelley___ann', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/kelley___ann', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// eight function
+function openEightPopup() {
+    var popup = window.open('https://bit.ly/riley-_reid', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/riley-_reid', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Add event listeners for each function on page load
+window.addEventListener('load', openFirstPopup);
+window.addEventListener('load', openSecondPopup);
+window.addEventListener('load', openThirdPopup);
+window.addEventListener('load', openFourthPopup);
+window.addEventListener('load', openFifthPopup);
+window.addEventListener('load', openSixthPopup);
+window.addEventListener('load', openSeventhPopup);
+window.addEventListener('load', openEightPopup);
