4eac0855b6b5e3f55a60f69c91b02571ea593eeb
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2024-02-27 14:58:02 +0100

    Create tankk.js

---
+++ tankk.js
@@ -0,0 +1,35 @@
+// First function
+function openFirstPopup() {
+    var popup = window.open('https://bit.ly/scarlette_jones', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/scarlette_jones', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Second function
+function openSecondPopup() {
+    var popup = window.open('https://bit.ly/euphorfun', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/euphorfun', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Add event listeners for each function on page load
+window.addEventListener('load', openFirstPopup);
+window.addEventListener('load', openSecondPopup);
